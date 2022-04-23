0a6481884a313d38e7ffbc9ccf1871e7fd63b951
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-04-23 13:09:50 -0400

    menu update

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -4,6 +4,40 @@ import Logo from "./header/Logo";
 export default function Header(props) {
   const { session, handleSession } = props;
 
+  const profileLinks = [
+    {
+      label: "Profile",
+      icon: "bi-person-badge-fill",
+      id: "header-profile-link",
+      action: () => {
+        alert("Profile clicked");
+      },
+    },
+    {
+      label: "Settings",
+      icon: "bi-gear-fill",
+      id: "header-settings-link",
+      action: () => {
+        alert("Settings clicked");
+      },
+    },
+  ];
+
+  const renderProfileLinks = profileLinks.map((link) => {
+    return (
+      <li>
+        <button
+          id={link.id}
+          class="dropdown-item d-flex gap-2 align-items-center"
+          onClick={link.action}
+        >
+          <i class={link.icon}></i>
+          {link.label}
+        </button>
+      </li>
+    );
+  });
+
   return (
     <header id="advcte-header" class="py-3 mb-3 border-bottom">
       <div
@@ -66,14 +100,14 @@ export default function Header(props) {
         </div>
 
         <div class="d-flex align-items-center">
-          <form class="w-100 me-3">
+          {/* <form class="w-100 me-3">
             <input
               type="search"
               class="form-control"
               placeholder="Search..."
               aria-label="Search"
             />
-          </form>
+          </form> */}
 
           <div class="flex-shrink-0 dropdown">
             <a
@@ -84,41 +118,26 @@ export default function Header(props) {
               aria-expanded="false"
               data-bs-auto-close="outside"
             >
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="32"
-                height="32"
-                class="rounded-circle"
-              />
+              <i class="bi-person-circle" width="32" height="32"></i>
             </a>
             <ul
-              class="dropdown-menu text-small shadow"
+              class={
+                "dropdown-menu mx-0 border-0 shadow" +
+                (session.experience.darkMode ? "dropdown-menu-dark" : "")
+              }
               aria-labelledby="dropdownUser2"
             >
-              <li>
-                <a class="dropdown-item" href="index.html">
-                  New project...
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="index.html">
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="index.html">
-                  Profile
-                </a>
-              </li>
+              {/* Profile Links from Array */}
+              {renderProfileLinks}
+
               {/* Dark Mode Toggle */}
               <li>
-                <div class="dropdown-item form-check form-switch">
-                  <label class="form-check-label" for="darkToggle">
+                <div class="dropdown-item d-flex gap-2 align-items-center form-check form-switch">
+                  <label class="form-check-label no-select" for="darkToggle">
                     {session.experience.darkMode ? "Light Mode" : "Dark Mode"}
                   </label>
                   <input
-                    class="form-check-input"
+                    class="form-check-input secondary"
                     type="checkbox"
                     role="switch"
                     id="darkToggle"
