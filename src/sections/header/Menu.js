c81b861474ef6a536513bd712c896148ce60d0d3
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-04-23 00:20:30 -0400

    Update Menu.js

--- src/sections/header/Menu.js
+++ src/sections/header/Menu.js
@@ -7,7 +7,7 @@ export default function Menu(props) {
     <div id="advcte-header-menu" className="col-8 border">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="advcte.netlify.app">
             Navbar
           </a>
           <button
@@ -24,19 +24,23 @@ export default function Menu(props) {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="advcte.netlify.app"
+                >
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="advcte.netlify.app">
                   Link
                 </a>
               </li>
               <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle"
-                  href="#"
+                  href="advcte.netlify.app"
                   id="navbarDropdown"
                   role="button"
                   data-bs-toggle="dropdown"
@@ -46,12 +50,12 @@ export default function Menu(props) {
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="advcte.netlify.app">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="advcte.netlify.app">
                       Another action
                     </a>
                   </li>
@@ -59,7 +63,7 @@ export default function Menu(props) {
                     <hr class="dropdown-divider" />
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="advcte.netlify.app">
                       Something else here
                     </a>
                   </li>
@@ -68,7 +72,7 @@ export default function Menu(props) {
               <li class="nav-item">
                 <a
                   class="nav-link disabled"
-                  href="#"
+                  href="advcte.netlify.app"
                   tabindex="-1"
                   aria-disabled="true"
                 >
