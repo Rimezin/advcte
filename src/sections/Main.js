f527c1b7e5e92d78a8065670b4f9119092ce6312
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-04-23 00:11:22 -0400

    Update Main.js

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -4,11 +4,11 @@ export default function Main(props) {
   //const { session } = props;
 
   return (
-    <div id="advcte-main" className="row mh-100">
-      <div id="advcte-main-action" className="col-2 border">
+    <div id="advcte-main" className="row h-100">
+      <div id="advcte-main-action" className="col-2 h-100 border">
         Action Bar
       </div>
-      <div id="advcte-main-content" className="col-10 border">
+      <div id="advcte-main-content" className="col-10 h-100 border">
         Main Content
       </div>
     </div>
