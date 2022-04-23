1d846e8f479b46299f0d3944274ae25ec583ead4
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-04-23 14:44:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,37 @@ import React from "react";
 import Splash from "./sections/Splash";
 import Header from "./sections/Header";
 
-export default function App() {
-  //// Master States ////
-  /* session: [object] Holds session details for the user.
-   */
-  const [session, setSession] = React.useState({
-    loggedOn: true,
-    user: {
-      firstName: null,
-      lastName: null,
-      fullName: (firstName, lastName) => {
-        if (firstName === null || lastName === null) {
-          return null;
-        } else {
-          return `${firstName} ${lastName}`;
-        }
-      },
-      userName: null,
-      userId: null,
-      security: {
-        roleId: null,
-        rolePermissions: null,
-        navId: null,
-        navViews: null,
-      },
+//// Master States ////
+/* session: [object] Holds session details for the user.
+ */
+const [session, setSession] = React.useState({
+  loggedOn: true,
+  user: {
+    firstName: null,
+    lastName: null,
+    fullName: (firstName, lastName) => {
+      if (firstName === null || lastName === null) {
+        return null;
+      } else {
+        return `${firstName} ${lastName}`;
+      }
     },
-    experience: {
-      language: "english",
-      darkMode: true,
+    userName: null,
+    userId: null,
+    security: {
+      roleId: null,
+      rolePermissions: null,
+      navId: null,
+      navViews: null,
     },
-  });
+  },
+  experience: {
+    language: "english",
+    darkMode: true,
+  },
+});
 
+export default function App() {
   //// Functions ////
 
   // Handle the session //
