a0026997b8693af7912e1cf07ee6aeeb054e5740
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-04-25 21:05:14 -0400

    re-trying breadcrumb

---
+++ src/assets/Breadcrumb.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Breadcrumb(props) {
+  const { session, page } = props;
+
+  let crumbs = () => {
+    switch (page) {
+      case "home":
+        return ["home"];
+      default:
+        break;
+    }
+  };
+
+  let renderCrumbs = crumbs.map((crumb) => {
+    return (
+      <li
+        class={`breadcrumb-item ${page === crumb ? "active" : ""}`}
+        aria-current="page"
+      >
+        {crumb}
+      </li>
+    );
+  });
+
+  return (
+    <div aria-label="breadcrumb">
+      <ol
+        class={`breadcrumb ${
+          session.experience.darkMode ? "bg-secondary" : ""
+        }`}
+      >
+        {renderCrumbs}
+      </ol>
+    </div>
+  );
+}
